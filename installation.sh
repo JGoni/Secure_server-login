28d0866af1b61dad6404ca9e4e1ff0926e78426a
JGoni <jesika.goni.652@my.csun.edu>
2019-03-27 01:12:47 -0700

    installation of php

--- installation.sh
+++ installation.sh
@@ -17,3 +17,8 @@ sudo apt install apache2
 
 #install MySQL database management system
 sudo apt install mysql-server
+
+#install PHP incorporated with Apache and Mysql
+sudo apt install php libapache2-mod-php php-mysql
+#install php-cli for commnadline interpretation for PHP scripting language
+sudo apt install php-cli
