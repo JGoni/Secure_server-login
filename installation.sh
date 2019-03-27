0f2eb6f62d6f8ac6613764ee45da9603ab58d5d4
JGoni <jesika.goni.652@my.csun.edu>
2019-03-27 00:44:06 -0700

    installation of apache2

--- installation.sh
+++ installation.sh
@@ -10,3 +10,7 @@ sudo apt-get install openssh-server
 
 #install SSH client to connect and controll from here
 sudo apt-get install openssh-client
+
+#install Apache web servwe 
+sudo apt update
+sudo apt install apache2
