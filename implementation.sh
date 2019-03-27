842c0e2a563ccd5d91bb41e851d9d020a5d60cba
JGoni <jesika.goni.652@my.csun.edu>
2019-03-27 00:01:37 -0700

    installation and implementation of ufw

---
+++ implementation.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+#enable/disable ufw
+sudo ufw enable
---
+++ installation.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+#ufw is Uncomplicated Firewall, front-end to iptables
+#manage firewall simple, easy use
+sudo apt-get install ufw
