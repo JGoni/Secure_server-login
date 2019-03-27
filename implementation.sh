08f03124fc6b6b57fd642f0d1001c1d939097a5d
JGoni <jesika.goni.652@my.csun.edu>
2019-03-27 00:13:58 -0700

    added some Firewall rules to be implemented

--- implementation.sh
+++ implementation.sh
@@ -2,3 +2,9 @@
 
 #enable/disable ufw
 sudo ufw enable
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw allow https
+sudo ufw allow www
+sudo ufw allow ftp
